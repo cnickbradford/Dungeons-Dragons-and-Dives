97eef1d548f4e3ac6300cee9c3e341558052cf72
nick bradford <cnickbradford@gmail.com>
2023-03-17 09:08:41 -0500

    saving and deleting spells function created, styling added

--- README.md
+++ README.md
@@ -1,5 +1,33 @@
-# Dungeons-Dragons-and-Dives-
+Dungeons, Dragons and Drives
 
-Welcome Traveler to Dungeons, Dragons, and Dives. At its conception, This webpage will be a basic spell book application made by yours truly. 
+"Dungeons, Dragons and Drives" is a web application that allows users to search for Dungeons and Dragons 5E spells by class, spell level, or school of magic. It uses the D&D 5th Edition API to fetch spell data, and Bootstrap as a CSS library for styling.
+Getting Started
 
-I hope in the future, that I can add a chacter creation element to the webppage, but for now, please enjoy your spellbook!
+To use "Dungeons, Dragons and Drives", simply visit the webpage and use the search bar to start finding spells that are perfect for your campaign!
+
+"Dungeons, Dragons and Drives" currently supports the following features:
+
+    Search for spells by class, spell level, or school of magic
+    View spell details, including spell description, duration, and casting time
+    Save your favorite spells to prepare a spell list or for future reference
+
+Technologies Used
+
+"Dungeons, Dragons and Drives" was built using the following technologies:
+
+    HTML/CSS/JavaScript
+    jQuery for AJAX requests and DOM manipulation
+    Bootstrap for styling
+    D&D 5th Edition API for spell data
+
+Future Plans
+
+In the future, "Dungeons, Dragons and Drives" may support additional features such as:
+
+    Sorting spells by name or casting time
+    Filtering spells by range or components
+    Saving spells to a user's favorites list
+
+Contributing
+
+If you'd like to contribute to "Dungeons, Dragons and Drives", feel free to fork the repository and submit a pull request. 
--- javascript/app.js
+++ javascript/app.js
@@ -1,7 +1,15 @@
 let $noSpell = $('<div>No spells meet search criteria, Please try again</div>)')
-let $opening = $(`<div>Search for spells by spell level</div`)
+let $opening = $(`<h3 class ="result-card" >You can search for spells by spell level, class, or school</h3>
+<h2 class = 'saved-card'>You can also save the spells once you search and can view all saved spells!</h2>`)
 let $classes = ["barbarian", "bard", "cleric", "druid", "fighter", "monk", "paladin", "ranger", "rogue", "sorceror", "warlock", "wizard"]
 let spellData = []
+let spellLevelArr = []
+// let spellLevelCount = {};
+
+$("#results").append($opening)
+$("#showspell").on("click", displaysavedSpells)
+
+
 
 
 
@@ -82,7 +90,8 @@ function gatherSpellsbySchool() {
 //function that will display the searched spells to the user and add buttons to each spell
 function displaySpells() {
     console.log(spellData)
-    //$open.hide() && $noShow.hide();
+    $('#results').children().remove()
+    $('#homeimg').remove()
     for (let i = 0; i < spellData.length; i++) {
         let spellClasses = []
         let $card = $(`<span class="result-card">
@@ -100,9 +109,10 @@ function displaySpells() {
         for (let j = 0; j < spellData[i].classes.length; j++) {
             spellClasses.push(spellData[i].classes[j].name)
         }
+        //adding the buttons to save the spell to each result card 
         let addedClasses = (`<h3 class = "spell-genres">${spellClasses.join(", ")}</h3>`)
         $card.append(addedClasses)
-        let $saveButton = $(`<button id = ${i} class = "save">Save spell</button>`);
+        let $saveButton = $(`<button id = ${i} class = "submit">Save spell</button>`);
         $saveButton.on("click", saveSpell)
         $card.append($saveButton)
         $('#results').append($card);
@@ -111,12 +121,78 @@ function displaySpells() {
 
 }
 
+//function that that gathers all of the levels of the spells you have saved and finds how many times that level appears 
+// function totalSpellLevels(){
+//     spellLevelArr = [];
+//     spellLevelCount = {};
+//     for (let i = 0; i < localStorage.length; i++) {
+//       let spellLevel = localStorage.key(i).split('')
+//       spellLevelArr.push(spellLevel[spellLevel.length - 2])
+//     }
+//     for(let j = 0; j < spellLevelArr.length;j++ ){
+//         let num = spellLevelArr[j]
+//         if(spellLevelCount[num]){
+//             spellLevelCount[num]++
+//         }else{
+//             spellLevelCount[num] = 1
+//         }
+//     }
+//     displaySpellLevels()
+// }
+
+
+//function that displays the total of each spell level you have saved
+// function displaySpellLevels(){
+//     let $card = $(`<span class ="saved-card">
+//     <h2 class = "spell-title"> Saved spell levels </h2>
+//     <ul id = "level-list">
+//     </ul>
+//     </span>`)
+//     for(key in spellLevelCount){
+//         console.log(spellLevelCount[key])
+//         console.log(key)
+//          let count = $(`<li>${spellLevelCount[key]} Level ${key}</li>`)
+//          $("#level-list").append(count)
+//          $("#level-counts").append($card)
+//     }
+// }
+
+
+
+//adding grid function to view saved spells button 
+function displaysavedSpells() {
+    $('#savedSpells').children().remove()
+    $('#homeimg').remove()
+    for (let i = 0; i < localStorage.length; i++) {
+        let $card = $(`<span class = "saved-card">
+    <h3 class = "spell-title">${localStorage.key(i)}</h3>
+    <em>Summary:</em>
+    ${localStorage.getItem(localStorage.key(i))}
+    </span>`)
+        let $deleteButton = $(`<button id = ${i} class = "delete">Delete spell</button>`);
+        $deleteButton.on("click", deleteSpell)
+        $card.append($deleteButton)
+        $(`#savedSpells`).append($card)
+    }
+    //totalSpellLevels()
+}
+
+
 
 
 //function that saves the selected spell to local storage
 function saveSpell(e) {
     let savedSpell = spellData[e.target.id]
-    localStorage.setItem(`${savedSpell.name}`, `Level ${savedSpell.level}`)
+    console.log(savedSpell.desc[0])
+    localStorage.setItem(`${savedSpell.name} Level ${savedSpell.level}`, `${savedSpell.desc}`)
+}
+
+//function that deletes spell from local storage and re-runs view saved function, refreshing the list
+function deleteSpell(e) {
+    console.log(e.target.id)
+    localStorage.removeItem(localStorage.key(e.target.id))
+    displaysavedSpells()
+
 }
 
 
